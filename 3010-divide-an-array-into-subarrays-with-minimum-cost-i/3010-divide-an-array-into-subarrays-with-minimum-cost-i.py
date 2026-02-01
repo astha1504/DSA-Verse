b22f4615e542a914e5f95dd4b0d4fbcbc29b0b33
Astha Singh <asthasingh00442@gmail.com>
2026-02-01 22:13:19 +0530

    Time: 3 ms (35.86%), Space: 19.5 MB (15.15%) - LeetHub

---
+++ 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minimumCost(self, nums: List[int]) -> int:
+        base = nums[0]
+        smallest1 = float('inf')
+        smallest2 = float('inf')
+        
+        for x in nums[1:]:
+            if x < smallest1:
+                smallest2 = smallest1
+                smallest1 = x
+            elif x < smallest2:
+                smallest2 = x
+        return base + smallest1 + smallest2
