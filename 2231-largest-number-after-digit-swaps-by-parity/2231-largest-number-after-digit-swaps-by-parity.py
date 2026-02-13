cb21a7bf4720ef82cddd8a22e3e149cae8b56199
Astha Singh <asthasingh00442@gmail.com>
2026-02-13 13:50:32 +0530

    Time: 0 ms (100%), Space: 19.3 MB (65.33%) - LeetHub

---
+++ 2231-largest-number-after-digit-swaps-by-parity/2231-largest-number-after-digit-swaps-by-parity.py
@@ -0,0 +1,10 @@
+class Solution:
+    def largestInteger(self, num: int) -> int:
+        arr=list(str(num))
+        n=len(arr)
+        for i in range(n):
+            for j in range(i+1,n):
+                if (int(arr[i])%2==int(arr[j])%2) and arr[j]>arr[i]:
+                    arr[i], arr[j] = arr[j], arr[i]
+        return int("".join(arr))
+        
