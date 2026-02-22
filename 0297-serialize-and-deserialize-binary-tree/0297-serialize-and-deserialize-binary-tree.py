3a5984c02369f9e273053c6a50c703e53ce33434
Astha Singh <asthasingh00442@gmail.com>
2026-02-22 19:38:41 +0530

    Time: 84 ms (74.66%), Space: 22.8 MB (28.14%) - LeetHub

---
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
@@ -0,0 +1,28 @@
+class Codec:
+    def serialize(self, root):
+        """Encodes a tree to a single string."""
+        res=[]
+        def dfs(node):
+            if not node:
+                res.append("N")
+                return
+            res.append(str(node.val))
+            dfs(node.left)
+            dfs(node.right)
+        dfs(root)
+        return ",".join(res)
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree."""
+        items=data.split(',')
+        self.i=0
+        def dfs():
+            if items[self.i]=="N":
+                self.i+=1
+                return None
+            node=TreeNode(int(items[self.i]))
+            self.i+=1
+            node.left=dfs()
+            node.right=dfs()
+            return node
+        return dfs()
