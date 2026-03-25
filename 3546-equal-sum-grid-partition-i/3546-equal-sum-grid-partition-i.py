74c54eaacf0d7a477c203a391047e3b4fcf4b431
Astha Singh <asthasingh00442@gmail.com>
2026-03-25 14:19:20 +0530

    Time: 160 ms (50.69%), Space: 45.4 MB (54.11%) - LeetHub

--- 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.py
+++ 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.py
@@ -2,27 +2,26 @@ class Solution:
     def canPartitionGrid(self, grid: List[List[int]]) -> bool:
         m=len(grid)
         n=len(grid[0])
-        total=0
         rowsum=[0]*m
         colsum=[0]*n
 
         for i in range (m):
             for j in range (n):
-                total+=grid[i][j]
+
                 rowsum[i]+=grid[i][j]
                 colsum[j]+=grid[i][j]
-        if total%2!=0:
+        if sum(rowsum)%2!=0:
             return False
-
-        upper=0
-        for i in range (m-1):
-            upper+=rowsum[i]
-            if upper==total-upper:
+        r=sum(rowsum)//2
+        c=sum(colsum)//2
+        a=0
+        for i in rowsum:
+            a+=i
+            if a==r:
                 return True
-        
-        left=0
-        for j in range (n-1):
-            left+=colsum[j]
-            if left==total-left:
-                return True
-        return False
+        a=0
+        for i in colsum:
+            a+=i
+            if a==c:
+                return True     
+        return False       
