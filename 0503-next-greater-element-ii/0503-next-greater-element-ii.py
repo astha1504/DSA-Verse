a2a0acefde306ef08475ee6c0b6f9b0f7c485cdf
Astha Singh <asthasingh00442@gmail.com>
2026-04-09 12:01:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.py
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.py
@@ -1,13 +1,41 @@
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
-        n=len(nums)
-        ans=[-1]*n
-        stack=[]
-        for i in range(2*n-1,-1,-1):
-            while stack and stack[-1]<=nums[i%n]:
-                stack.pop()  
-            if i<n:    
-                if stack:
-                    ans[i]=stack[-1]
-            stack.append(nums[i%n])
-        return ans    
+        m={}
+        st=[]
+        for i in range(len(nums)-1):
+            st.append(nums[i])
+        for i in range(len(nums)-1,-1,-1):
+            while st and nums[i]>=st[-1]:
+                from typing import List
+
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        ans = [-1] * n
+        st = []
+        
+        # First pass: preload stack with elements for circularity
+        for i in range(n - 1, -1, -1):
+            st.append(nums[i])
+        
+        # Second pass: actual computation
+        for i in range(n - 1, -1, -1):
+            while st and st[-1] <= nums[i]:
+                st.pop()
+            if st:
+                ans[i] = st[-1]
+            else:
+                ans[i] = -1
+            st.append(nums[i])
+        
+        return ans
+st.pop()
+            
+            if not st:
+                m[nums[i]]=-1
+            else:
+                m[nums[i]]=st[-1]
+            
+            st.append(nums[i])
+        
+        return [m[x] for x in nums]
